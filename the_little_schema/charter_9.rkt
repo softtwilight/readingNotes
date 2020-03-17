6778780328a6144b6b70c12088b07206cae3dc14
softtwilight <tjuyxl@gmail.com>
2020-03-17 23:30:26 +0900

    chapter9

--- the_little_schema/charter_9.rkt
+++ the_little_schema/charter_9.rkt
@@ -26,11 +26,43 @@
 ;; it can't stop!! otherwise, if the last-try willn't stop, the (will-stop? last-try)
 ;; return, so is the method last-try return, which means it will stop.
 ;; Oh WTF!
-(define last-try
-  (lambda (x)
-    (and (will-stop? last-try)
-         (eternity x))))
+;(define last-try
+;  (lambda (x)
+;    (and (will-stop? last-try)
+;         (eternity x))))
+(define (eternity x)
+  (eternity x))
 
 ;; 这是唯一一个我们能描述却不能定义的问题。
 
 ;; define 的含义。
+(define length
+  (lambda (l)
+    (cond ((null? l) 0)
+    (else (+ 1 (length (cdr l)))))))
+
+;; 不用define，我们怎么写length呢
+;; 只能计算length = 0 的函数
+((lambda (l)
+  (cond ((null? l) 0)
+        (else (+ 1
+                 (eternity (cdr l))))))
+ '()) ;; -> 0
+
+;; 只能计算length = 1 的函数
+
+(define length1 (lambda (l)
+  (cond ((null? l) 0)
+        (else
+         (+ 1
+            ((lambda (l)
+               (cond
+                 ((null? l) 0)
+                 (else
+                  (+ 1
+                     (eternity (cdr l))))))
+             (cdr l)))))))
+
+(length1 (list 'a))
+
+
